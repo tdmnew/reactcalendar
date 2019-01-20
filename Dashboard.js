200d100e0446657af249f9b6a3950f57cb6321b5
Tim <timothy.david.new@hotmail.co.uk>
2019-01-20 17:21:35 +0000

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Calendar from '.components/Calendar';
-import { getDateProps } from './utils/date'; 
+import Calendar from './src/components/Calendar';
+import { getDateProps } from './src/utils/date'; 
 
 class Dashboard extends Component {
     constructor(){
